7f4e42eb8c754219834eaadde66843c518b24d88
viniciusG98 <viniciusG98@users.noreply.github.com>
2019-05-27 06:45:49 -0400

    todo-app

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -17,38 +17,53 @@ export default class Todo extends Component {
         this.handleRemove = this.handleRemove.bind(this)
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
+        this.handleClear = this.handleClear.bind(this)
 
         this.refresh()
     }
 
-    refresh() {
-        axios.get(`${URL}?sort=-createdAt`)
-            .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+    refresh(description = '') {
+        const search = description ? `&description__regex=/${description}/` : ''
+        axios.get(`${URL}?sort=-createdAt${search}`)
+            .then(resp => this.setState({ ...this.state, description, list: resp.data }))
+    }
+
+    handleSearch() {
+        this.refresh(this.state.description)
+    }
+
+    handleClear() {
+        this.refresh()
     }
 
     handleChange(e) {
-        this.setState({...this.state, description: e.target.value })
+        this.setState({ ...this.state, description: e.target.value })
     }
 
     handleAdd() {
-        const description = this.state.description
-         axios.post(URL, {description})
-             .then(resp => this.refresh())
+         if (this.state.description == '') {
+             alert('Por favor insira sua tarefa!')
+         } else {
+            const description = this.state.description
+            axios.post(URL, { description })
+                .then(resp => this.refresh())
+         }
     }
 
-    handleMarkAsDone(todo){
-        axios.put(`${URL}/${todo._id}`, { ...todo, done: true})
-            .then(resp => this.refresh())
+    handleMarkAsDone(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+            .then(resp => this.refresh(this.state.description))
     }
 
     handleMarkAsPending(todo) {
-        axios.put(`${URL}/${todo._id}`, { ...todo, done: false})
-            .then(resp => this.refresh())
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
+            .then(resp => this.refresh(this.state.description))
     }
 
     handleRemove(todo) {
         axios.delete(`${URL}/${todo._id}`)
-            .then(resp => this.refresh())
+            .then(resp => this.refresh(this.state.description))
     }
 
     render() {
@@ -59,12 +74,14 @@ export default class Todo extends Component {
                     description={this.state.description}
                     handleAdd={this.handleAdd}
                     handleChange={this.handleChange}
+                    handleSearch={this.handleSearch}
+                    handleClear={this.handleClear}
                 />
-                <TodoList 
-                list={this.state.list} 
-                handleRemove={this.handleRemove}
-                handleMarkAsDone={this.handleMarkAsDone}
-                handleMarkAsPending={this.handleMarkAsPending}
+                <TodoList
+                    list={this.state.list}
+                    handleRemove={this.handleRemove}
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPending={this.handleMarkAsPending}
                 />
             </div>
         )
