68e1b5d3124155e69c5415dc3ae96c9d7fd5c1eb
viniciusG98 <viniciusG98@users.noreply.github.com>
2019-05-26 22:56:19 -0400

    todo-app

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós' />
+
+        <h2>Nossa História</h2>
+        <p>Aplicação de lista usando React-js</p>
+        <h2>Missão e Visão</h2>
+        <p>Oferecer o melhor serviço para nossos clientes</p>
+        <h2>Imprensa</h2>
+        <p>Apresentamos a todos vocês o nosso brilhante produto!</p>
     </div>
 )
